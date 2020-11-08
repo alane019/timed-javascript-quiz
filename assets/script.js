b1374e13ee7df2d0d7653ac5680a40d5e7717680
Adam <alane019@gmail.com>
2020-11-07 19:55:10 -0500

    add script js

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+
+
+window.onload() = function() {
+
+   const questions = ["Is JavaScript a case-sensitive language?", " ...?"];
+
+   const answers =  ["Yes, JavaScript is a case sensitive language.", "...?"];
+
+
+   /*
+
+   while (answerList.firstChild) {
+         answerList.removeChild(answerList.firstChild);
+      }
+
+   */
+
+
+}
+
