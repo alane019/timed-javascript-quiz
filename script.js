c14b4211a96c7f9f5b94eb062c06e73f3a4ab526
Adam <alane019@gmail.com>
2020-11-08 03:42:14 -0500

    update timer and game ending

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 
 window.onload = function() {
-
 ///////////////////////////////////////
-
-   let questionNumber = 0;
-  /* const questionArray = [
-   {
-   "Is JavaScript a case-sensitive language?",
-   "Is jQuery a programming language?",
-   "Can you parse strings to interger values in javascript?"   
-   ]; */
 
   let questions = [
    { question: "This question will not appear.", answer: "This will not be used"},
@@ -18,7 +9,8 @@ window.onload = function() {
     { question: "You can parse strings to interger values in javascript?", answer: "T"},
     { question: "Stringify is a JSON method", answer: "T"},
     { question: "Bootstrap is a programming language", answer: "F"},
-    { question: "Parse is a JSON method", answer: "T"}
+    { question: "Parse is a JSON method", answer: "T"},
+    { question: "end", answer: "This will not be used"}
    ];
 
   //[not needed] let quizState = "ready"
@@ -26,30 +18,57 @@ window.onload = function() {
    let startButtonDiv = document.getElementById("start-btn");
    let questionDiv = document.getElementById("question-div");
    let currentItemHeading = document.getElementById("current-item-heading");
-
    const trueDiv = document.getElementById("true-div");
    const falseDiv = document.getElementById("false-div");
    const tfDiv = document.getElementById("tf-div");
-   /*
-      <div id="time-div" class=""></div>
-    <div id="number-correct" class=""></div>
-   */
-  let time = 120000; //2 minutes;
+   const pageContainer = document.getElementById("pageContainer");
+
+  let questionNumber = 0;
+  let time = 120; //2 minutes;
+  let timeDiv = document.getElementById("time-div");
   let numberCorrect = 0; 
-   const timeDiv = document.getElementById("time-div");
-   const numberCorrectDiv = document.getElementById("number-correct-div");
+  let numberCorrectDiv = document.getElementById("number-correct-div");
+  let timerOn = true;
+  let trueOrFalse = "";
+  let initials = "";
 
-   
-   trueOrFalse = "";
 
-   let showNextQuestion = function (){
+   let showNextQuestion = function(){
       questionNumber++;
-      //let displayNumber = questionNumber + 1;
       let q = questions[questionNumber];
+      if(q.question == "end"){
+         gameComplete();
+         return false;        
+      }
       trueOrFalse = q.answer;
       currentItemHeading.textContent = `Question ${questionNumber}`;
-      questionDiv.textContent =  q.question;
-            //update tfStatus based on tfAnswerDataArray....
+      questionDiv.textContent =  q.question;     
+   }
+
+   let gameComplete = function(){
+      pageContainer.style.backgroundColor = "rgb(32, 154, 24, 0.28)";
+      timerOn = false;
+      initials = prompt(`Enter your initials to save with score: `)
+      currentItemHeading.textContent = "Game complete!";
+      questionDiv.innerHTML = "<p>FINAL SCORE: " + numberCorrect + "</p> <p> Initials: " + initials + " </p> <p>   Good job! </p>";
+      localStorage.setItem("initials", initials);
+      localStorage.setItem("score", numberCorrect);
+   
+   }
+
+   // correct and incorrect functions
+   var incorrect = function(){
+      console.log("Incorrect");
+      alert("Incorrect.  \n 30 seconds will be substracted from the remaining time");
+      time = parseInt(time) - 30;
+   }
+
+   var correct = function(){
+      numberCorrect = numberCorrect + 1;
+      console.log(numberCorrect);
+      numberCorrectDiv.textContent = "Number correct: " + numberCorrect;
+      console.log("Correct");
+    //  alert("Correct.");
    }
 
   // True false button event handler
@@ -58,27 +77,22 @@ window.onload = function() {
       // cq is the current question
       let cq = questions[questionNumber];
       if(event.target.matches("button")) {
-        // [OK: message appears when button clicked]  alert("clicked");
+        // Correct answer is True.
          if(cq.answer == "T"){
             if(event.target.id == "true-div"){
-                console.log("Correct");
-                numberCorrect++;
-                numberCorrectDiv = numberCorrect;
+               correct();
             }
             else if(event.target.id == "false-div"){
-               console.log("Incorrect");
-               time = time - 30000;
+               incorrect();
            }
          }
-         /////
+          // Correct answer is False.
          if(cq.answer == "F"){
                if(event.target.id == "true-div"){
-                  console.log("Incorrect");
+                incorrect();
                }
                else if(event.target.id == "false-div"){
-                  console.log("Correct");
-                  numberCorrect++;
-                  numberCorrectDiv = numberCorrect;
+                correct();
             }
          }
       }  // end of: "target is button"
@@ -86,6 +100,7 @@ window.onload = function() {
    });
 
 
+   // Game start function
    startButtonDiv.addEventListener("click", function(event) {
       event.preventDefault();
       if(event.target.matches("button")) {
@@ -95,16 +110,32 @@ window.onload = function() {
           //[not needed]  startButton.visibility = "hidden";
           tfDiv.classList.remove("d-none");
           startButtonDiv.classList.add("d-none");
-       // var item = document.createElement("div");
-       // item.textContent = groceries[event.target.parentElement.id];
-       // shoppingCartEl.append(item);
+
+          let gameTimer = window.setInterval(function(){
+            if(timerOn != true){
+               clearInterval(gameTimer);
+            }
+
+            time = parseInt(time) - 1; 
+            timeDiv.textContent = "Seconds remaining: " + time;
+            numberCorrectDiv.textContent = "Number correct: " + numberCorrect;
+
+            if(time == 0){
+               gameComplete();
+               clearInterval(gameTimer);
+            }
+      
+         }, 1000);
+      
       }
    });
 
-   /* 
    
+
+   /* 
    while (answerList.firstChild) {
          answerList.removeChild(answerList.firstChild);
       }
    */
+
 } //end window onload
